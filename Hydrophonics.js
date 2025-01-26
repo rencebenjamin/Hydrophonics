4650bb4bd91ab0efc952f75dacbaec665b61c3b3
Lawwwww <laurencedec1434@gmail.com>
2025-01-26 17:09:25 +0800

    Hydrophonics.js

--- Hydrophonics.js
+++ Hydrophonics.js
@@ -14,8 +14,7 @@ function updateDateTime() {
     dateTimeElement.textContent = now.toLocaleDateString('en-US', options);
 }
 
-// Update the time every second
 setInterval(updateDateTime, 1000);
 
-// Initialize on page load
+
 updateDateTime();
