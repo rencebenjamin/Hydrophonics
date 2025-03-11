c5aca426d37191247073e2365cda9548cd0f4f41
Alexander <DalawangEdgedSword@proton.me>
2025-03-11 10:32:54 +0800

    initial push

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-function updateDateTime() {
-  const dateTimeElement = document.getElementById("dateTime");
-  if (!dateTimeElement) return;
 
-  const now = new Date();
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  };
-  dateTimeElement.textContent = now.toLocaleDateString("en-US", options);
-}
-
-setInterval(updateDateTime, 1000);
-updateDateTime();
 
 // Function to show sensor details
 function showDetails(sensorId) {
